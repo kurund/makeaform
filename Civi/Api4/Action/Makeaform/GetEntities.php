c2ac1dbbf73d15c629a292bfa334e219257d46f1
Kurund Jalmi <kurundjalmi@thirdsectordesign.org>
2025-12-29 14:41:46 +0000

    define fixed entities for now

--- Civi/Api4/Action/Makeaform/GetEntities.php
+++ Civi/Api4/Action/Makeaform/GetEntities.php
@@ -8,6 +8,7 @@ use Civi\AfformAdmin\AfformAdminMeta;
  * Get list of available entities for the MakeaForm builder.
  *
  * Returns entities that can be used to create submission forms.
+ * Only returns 'primary' type entities (not join entities like Address, Email, etc.)
  */
 class GetEntities extends \Civi\Api4\Generic\AbstractAction {
 
@@ -21,11 +22,29 @@ class GetEntities extends \Civi\Api4\Generic\AbstractAction {
 
   /**
    * Get list of entities from Afform's metadata
+   *
+   * Only returns 'primary' type entities that are suitable for creating
+   * standalone submission forms. Excludes 'join' entities which are for
+   * sub-records (Address, Email, Phone, etc.)
    */
   protected function getEntities(): array {
     // Use Afform's metadata which includes the proper list of supported entities
     $afformMeta = AfformAdminMeta::getMetadata();
     $entities = $afformMeta['entities'] ?? [];
+
+    // Define sort order for common entities (others will be sorted alphabetically after)
+    $sortOrder = [
+      'Individual' => 1,
+      'Organization' => 2,
+      'Household' => 3,
+      'Activity' => 4,
+      'Event' => 5,
+      'Membership' => 6,
+      'Participant' => 7,
+      'Contribution' => 8,
+      'Case' => 9,
+      'Relationship' => 10,
+    ];
 
     $entityMap = [];
     foreach ($entities as $name => $entity) {
@@ -34,13 +53,29 @@ class GetEntities extends \Civi\Api4\Generic\AbstractAction {
         continue;
       }
 
+      // Only include 'primary' type entities for form building
+      // Skip 'join' entities (Address, Email, Phone, Website, IM, LocBlock)
+      $entityType = $entity['type'] ?? 'primary';
+      if ($entityType !== 'primary') {
+        continue;
+      }
+
       $entityMap[$name] = [
         'name' => $entity['entity'] ?? $name,
         'title' => $entity['label'] ?? $name,
         'icon' => $entity['icon'] ?? NULL,
-        'type' => $entity['type'] ?? 'primary',
+        'type' => $entityType,
+        'sort_order' => $sortOrder[$name] ?? 100,
       ];
     }
+
+    // Sort by sort_order then alphabetically by title
+    uasort($entityMap, function($a, $b) {
+      if ($a['sort_order'] !== $b['sort_order']) {
+        return $a['sort_order'] <=> $b['sort_order'];
+      }
+      return strcasecmp($a['title'], $b['title']);
+    });
 
     return $entityMap;
   }
